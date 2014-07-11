f0fbe3449d694cae8204bddf48858f32a19fb57d
Rich Wareham <rjw57@cam.ac.uk>
2014-07-11 09:46:54 +0100

    add ffmpeg option to hdf5toraw

--- dtcwtfusion/hdf5toraw.py
+++ dtcwtfusion/hdf5toraw.py
@@ -13,8 +13,6 @@ Options:
     --fps=NUM                           Set frames per second. [default: 10]
     -g NAME, --group=NAME               Which group from the HDF5 should be rendered
                                         [default: median_shrink]
-    --print-size                        Do not generate output but print the size of
-                                        output frames.
 
     <hdf5>                              HDF5 file as produced by fusevideo.
     <output>                            If specified, file to write output to. If not
@@ -30,13 +28,8 @@ Video encoding:
                                         the current PATH value.
                                         [default: ffmpeg]
     --bitrate=BITRATE                   Bitrate to pass to ffmpeg binary. [default: 10M]
+    --fps=FPS                           Frame rate. [default: 10]
     --ffmpeg-extra-opts=OPTS            Extra options to pass to the ffmpeg binary.
-
-To use the ffmpeg utility to convert input.h5 into output.mp4, you can use the
-following command:
-
-    $ hdf5toraw input.h5 | ffmpeg -y -f rawvideo -pix_fmt rgb24 \
-            -s `hdf5toraw --print-size input.h5` -i - -r 10 -vb 15M output.mp4
 
 """
 
@@ -44,6 +37,8 @@ from __future__ import print_function, division, absolute_import
 
 import itertools
 import logging
+import shlex
+import subprocess
 import sys
 
 from docopt import docopt
@@ -63,16 +58,22 @@ def tonemap(array):
 
     return np.array(norm_array * 255, dtype=np.uint8)
 
-def write_output(fobj, fps, left, right=None, leftidxs=None, rightidxs=None,
-        print_size=False):
+def output_shape_for_frames(left, right=None):
     if right is None:
         output_shape = left.shape[:2]
-        n_frames = left.shape[2]
     else:
         output_shape = (
                 max(left.shape[0], right.shape[0]),
                 left.shape[1] + right.shape[1]
         )
+    return output_shape
+
+def write_output(fobj, fps, left, right=None, leftidxs=None, rightidxs=None):
+    output_shape = output_shape_for_frames(left, right)
+
+    if right is None:
+        n_frames = left.shape[2]
+    else:
         n_frames = min(left.shape[2], right.shape[2])
 
     output_frame = np.zeros(output_shape, dtype=np.uint8)
@@ -95,10 +96,6 @@ def write_output(fobj, fps, left, right=None, leftidxs=None, rightidxs=None,
 
         for cidx in xrange(3):
             rgba_output[:,:,cidx] = output_frame
-
-        if print_size:
-            print('{0[1]}x{0[0]}'.format(rgba_output.shape))
-            return
 
         fobj.write(np.ravel(rgba_output, order='C').tostring())
 
@@ -131,13 +128,39 @@ def main():
 
     logging.info('Writing "{1}" frames to "{0}"'.format(options['<output>'], options['--group']))
 
-    output_fobj = my_open(options['<output>']) if not options['--print-size'] else None
-
     if options['--comparison']:
-        write_output(output_fobj, fps,
-                left=input_frames, right=denoised_frames,
-                leftidxs=h5['processed_indices'],
-                print_size=options['--print-size'])
+        left_frames, right_frames = input_frames, denoised_frames
+        left_idxs = h5['processed_indices']
     else:
-        write_output(output_fobj, fps, denoised_frames,
-                print_size=options['--print-size'])
+        left_frames, right_frames = denoised_frames, None
+        left_idxs = None
+
+    # Handle wiring up ffmpeg
+    if options['--ffmpeg']:
+        # Base FFMPEG command
+        ffmpeg_cmd = [
+            options['--ffmpeg-binary'],
+            '-y', '-f', 'rawvideo', '-pix_fmt', 'rgb24',
+            '-s', '{0[1]}x{0[0]}'.format(output_shape_for_frames(left_frames, right_frames)),
+            '-i', '-', '-r', str(int(options['--fps'])), '-vb', options['--bitrate'],
+        ]
+
+        # Extend with any extra options
+        if options['--ffmpeg-extra-opts'] is not None:
+            ffmpeg_cmd.extend(shlex.split(options['--ffmpeg-extra-opts']))
+
+        # Write to output
+        ffmpeg_cmd.append(options['<output>'] if options['<output>'] is not None else '-')
+
+        logging.info('Using FFMPEG command line: {0}'.format(ffmpeg_cmd))
+
+        ffmpeg_subp = subprocess.Popen(ffmpeg_cmd, stdin=subprocess.PIPE)
+        output_fobj = ffmpeg_subp.stdin
+    else:
+        output_fobj = my_open(options['<output>'])
+
+    write_output(output_fobj, fps, left=left_frames, right=right_frames, leftidxs=left_idxs)
+
+    if ffmpeg_subp is not None:
+        output_fobj.close()
+        ffmpeg_subp.wait()
